837b17e82e46fba97f43293186a576fda4e82401
YUNA9627 <yuna2175@naver.com>
2024-08-09 14:52:29 +0900

    partner JS 작성

--- js/script.js
+++ js/script.js
@@ -4,6 +4,34 @@ let paginationUp = document.querySelector('.pagination .up');
 let paginationDown = document.querySelector('.pagination .down');
 let currentIdx = 0;
 let testimonialCount = testimonialsLists.length;
+let partnerList = document.querySelector('.partners_list')
+let partnerListWidth = 234;
+let partnerListCount = document.querySelectorAll('.partners_list li').length; 
+let partnerListLeft = 0;
+let partnerListTotalWidth = partnerListWidth * partnerListCount;
+let animation;
+
+partnerList.style.width = partnerListTotalWidth + 'px';
+
+
+function movePartnerList(){
+  // partnerListLeft = partnerListLeft - 5; (아래와 같은 문장)
+  partnerListLeft -= 2;
+  if(partnerListLeft === -partnerListTotalWidth/2){
+    partnerListLeft = 0;
+  }
+  partnerList.style.left = partnerListLeft + 'px';
+  animation = requestAnimationFrame(movePartnerList);
+}
+requestAnimationFrame(movePartnerList);
+
+partnerList.addEventListener('mouseenter',()=>{
+  cancelAnimationFrame(animation)
+})
+partnerList.addEventListener('mouseleave',()=>{
+  requestAnimationFrame(movePartnerList);
+})
+
 
 /*
 pagers를 클릭하면 할일
