037942917f1ea5c6ab1d3f41dbdd60013e170a7a
YUNA9627 <yuna2175@naver.com>
2024-08-09 11:02:38 +0900

    script 작성

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let pagination = document.querySelectorAll('.testimonials .pager a');
+let testimonialsLists = document.querySelectorAll('.testimonials_list li');
+let paginationUp = document.querySelector('.pagination .up');
+let paginationDown = document.querySelector('.pagination .down');
+
+/*
+pagers를 클릭하면 할일
+  모든 a에서 active를 제거하고, 클릭한 그 요소에 active를 추가
+*/
+
